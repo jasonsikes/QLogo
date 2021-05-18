c66b6cd086a84d3d297b623ed032acfd49a219e1
Jason Sikes <sikes@acm.org>
2021-05-17 19:04:33 -0700

    Added contants for error numbers.

--- error.cpp
+++ error.cpp
@@ -46,208 +46,208 @@ void Error::setKernel(Kernel *aKernel) { mainKernel = aKernel; }
 
 void Error::turtleOutOfBounds() {
   QString message("Turtle out of bounds");
-  mainKernel->registerError(new Error(3, message), true);
+  mainKernel->registerError(new Error(ERR_TURTLE_BOUNDS, message), true);
 }
 
 DatumP Error::doesntLike(DatumP who, DatumP what, bool allowErract,
                          bool allowRecovery) {
   QString message("%1 doesn't like %2 as input");
   message = message.arg(who.showValue()).arg(what.showValue());
-  return mainKernel->registerError(new Error(4, message), allowErract,
+  return mainKernel->registerError(new Error(ERR_DOESNT_LIKE, message), allowErract,
                                    allowRecovery);
 }
 
 void Error::didntOutput(DatumP src, DatumP dest) {
   QString message("%1 didn't output to %2");
   message = message.arg(src.showValue()).arg(dest.showValue());
-  mainKernel->registerError(new Error(5, message), true);
+  mainKernel->registerError(new Error(ERR_DIDNT_OUTPUT, message), true);
 }
 
 void Error::notEnough(DatumP dest) {
   QString message("not enough inputs to %1");
   message = message.arg(dest.showValue());
-  mainKernel->registerError(new Error(6, message));
+  mainKernel->registerError(new Error(ERR_NOT_ENOUGH_INPUTS, message));
 }
 
 void Error::tooMany(DatumP dest) {
   QString message("too many inputs to %1");
   message = message.arg(dest.showValue());
-  mainKernel->registerError(new Error(8, message));
+  mainKernel->registerError(new Error(ERR_TOO_MANY_INPUTS, message));
 }
 
 void Error::dontSay(DatumP datum) {
   QString message("You don't say what to do with %1");
   message = message.arg(datum.showValue());
-  mainKernel->registerError(new Error(9, message));
+  mainKernel->registerError(new Error(ERR_DONT_SAY, message));
 }
 
 void Error::parenNf() {
   QString message("')' not found");
-  mainKernel->registerError(new Error(10, message));
+  mainKernel->registerError(new Error(ERR_PAREN_NF, message));
 }
 
 DatumP Error::noValueRecoverable(DatumP datum) {
   QString message("%1 has no value");
   message = message.arg(datum.showValue());
-  return mainKernel->registerError(new Error(11, message), true, true);
+  return mainKernel->registerError(new Error(ERR_NO_VALUE, message), true, true);
 }
 
 void Error::noValue(DatumP datum) {
   QString message("%1 has no value");
   message = message.arg(datum.showValue());
-  mainKernel->registerError(new Error(11, message));
+  mainKernel->registerError(new Error(ERR_NO_VALUE, message));
 }
 
 void Error::noHow(DatumP dest) {
   QString message("I don't know how to %1");
   message = message.arg(dest.showValue());
-  mainKernel->registerError(new Error(13, message));
+  mainKernel->registerError(new Error(ERR_NO_HOW, message));
 }
 
 DatumP Error::noHowRecoverable(DatumP dest) {
   QString message("I don't know how to %1");
   message = message.arg(dest.showValue());
-  return mainKernel->registerError(new Error(13, message), true, true);
+  return mainKernel->registerError(new Error(ERR_NO_HOW, message), true, true);
 }
 
 void Error::procDefined(DatumP procname) {
   QString message("%1 is already defined");
   message = message.arg(procname.showValue());
-  mainKernel->registerError(new Error(15, message));
+  mainKernel->registerError(new Error(ERR_ALREADY_DEFINED, message));
 }
 
 void Error::isPrimative(DatumP procname) {
   QString message("%1 is a primitive");
   message = message.arg(procname.showValue());
-  mainKernel->registerError(new Error(22, message));
+  mainKernel->registerError(new Error(ERR_IS_PRIMATIVE, message));
 }
 
 void Error::toInProc(DatumP cmd) {
   QString message("can't use %1 inside a procedure");
   message = message.arg(cmd.showValue());
-  mainKernel->registerError(new Error(23, message));
+  mainKernel->registerError(new Error(ERR_TO_IN_PROC, message));
 }
 
 void Error::toInPause(DatumP cmd) {
   QString message("Can't use %1 within PAUSE");
   message = message.arg(cmd.showValue());
-  mainKernel->registerError(new Error(19, message));
+  mainKernel->registerError(new Error(ERR_TO_IN_PAUSE, message));
 }
 
 void Error::unexpectedCloseSquare() {
   QString message("unexpected ']'");
-  mainKernel->registerError(new Error(26, message));
+  mainKernel->registerError(new Error(ERR_UNEXPECTED_SQUARE, message));
 }
 
 void Error::unexpectedCloseBrace() {
   QString message("unexpected '}'");
-  mainKernel->registerError(new Error(27, message));
+  mainKernel->registerError(new Error(ERR_UNEXPECTED_BRACE, message));
 }
 
 void Error::unexpectedCloseParen() {
   QString message("unexpected ')'");
-  mainKernel->registerError(new Error(12, message));
+  mainKernel->registerError(new Error(ERR_UNEXPECTED_PAREN, message));
 }
 
 void Error::alreadyDribbling() {
   QString message("already dribbling");
-  mainKernel->registerError(new Error(17, message), true);
+  mainKernel->registerError(new Error(ERR_ALREADY_DRIBBLING, message), true);
 }
 
 void Error::fileSystem() {
   QString message("File system error");
-  mainKernel->registerError(new Error(18, message));
+  mainKernel->registerError(new Error(ERR_FILESYSTEM, message));
 }
 
 DatumP Error::fileSystemRecoverable() {
   QString message("File system error");
-  return mainKernel->registerError(new Error(18, message), true, true);
+  return mainKernel->registerError(new Error(ERR_FILESYSTEM, message), true, true);
 }
 
 void Error::listHasMultExp(DatumP list) {
   QString message("Runlist %1 has more than one expression");
   message = message.arg(list.showValue());
-  mainKernel->registerError(new Error(43, message));
+  mainKernel->registerError(new Error(ERR_LIST_HAS_MULTIPLE_EXPRESSIONS, message));
 }
 
 void Error::alreadyOpen(DatumP what) {
   QString message("File %1 already open");
   message = message.arg(what.showValue());
-  mainKernel->registerError(new Error(41, message), true);
+  mainKernel->registerError(new Error(ERR_ALREADY_OPEN, message), true);
 }
 
 void Error::cantOpen(DatumP what) {
   QString message("I can't open file %1");
   message = message.arg(what.showValue());
-  mainKernel->registerError(new Error(40, message), true);
+  mainKernel->registerError(new Error(ERR_CANT_OPEN, message), true);
 }
 
 void Error::notOpen(DatumP what) {
   QString message("File %1 not open");
   message = message.arg(what.showValue());
-  mainKernel->registerError(new Error(42, message), true);
+  mainKernel->registerError(new Error(ERR_NOT_OPEN, message), true);
 }
 
 void Error::alreadyFilling() {
   QString message = "Already filling";
-  mainKernel->registerError(new Error(45, message), true);
+  mainKernel->registerError(new Error(ERR_ALREADY_FILLING, message), true);
 }
 
 void Error::noGraphics() {
   QString message = "Graphics not initialized";
-  mainKernel->registerError(new Error(28, message), true);
+  mainKernel->registerError(new Error(ERR_NO_GRAPHICS, message), true);
 }
 
 DatumP Error::noTest(DatumP what) {
   QString message = "%1 without TEST";
   message = message.arg(what.showValue());
-  return mainKernel->registerError(new Error(25, message), true, true);
+  return mainKernel->registerError(new Error(ERR_NO_TEST, message), true, true);
 }
 
 void Error::notInsideProcedure(DatumP what) {
   QString message = "Can only use %1 inside a procedure";
   message = message.arg(what.showValue());
-  mainKernel->registerError(new Error(31, message));
+  mainKernel->registerError(new Error(ERR_NOT_INSIDE_PROCEDURE, message));
 }
 
 DatumP Error::macroReturned(DatumP aOutput) {
   QString message = "Macro returned %1 instead of a list";
   message = message.arg(aOutput.showValue());
-  return mainKernel->registerError(new Error(29, message), true, true);
+  return mainKernel->registerError(new Error(ERR_MACRO_RETURNED_NOT_LIST, message), true, true);
 }
 
 DatumP Error::insideRunresult(DatumP cmdName) {
   QString message = "Can't use %1 inside RUNRESULT";
   message = message.arg(cmdName.showValue());
-  return mainKernel->registerError(new Error(38, message), true, true);
+  return mainKernel->registerError(new Error(ERR_INSIDE_RUNRESULT, message), true, true);
 }
 
 DatumP Error::noApply(DatumP what) {
   QString message = "Can't use %1 without APPLY";
   message = message.arg(what.showValue());
-  return mainKernel->registerError(new Error(44, message), true, true);
+  return mainKernel->registerError(new Error(ERR_NO_APPLY, message), true, true);
 }
 
 void Error::stackOverflow()
 {
   QString message("Stack overflow");
-  mainKernel->registerError(new Error(2, message));
+  mainKernel->registerError(new Error(ERR_STACK_OVERFLOW, message));
 }
 
 void Error::throwError(DatumP aTag, DatumP aOutput) {
   Error *e;
   if (aTag.wordValue()->keyValue() == "ERROR") {
     if (aOutput == nothing) {
-      e = new Error(21, "Throw \"Error");
+      e = new Error(ERR_THROW, "Throw \"Error");
       e->tag = aTag;
     } else {
-      e = new Error(35, aOutput);
+      e = new Error(ERR_CUSTOM_THROW, aOutput);
       e->tag = aTag;
     }
   } else {
     QString message = "Can't find catch tag for %1";
     message = message.arg(aTag.showValue());
-    e = new Error(14, message);
+    e = new Error(ERR_NO_CATCH, message);
     e->tag = aTag;
     e->output = aOutput;
   }
